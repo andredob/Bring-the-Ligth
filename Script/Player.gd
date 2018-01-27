e118e5a74742528c80bac8c2bd77ccaa4ddd1dab
mikeask <31136739+mikeask@users.noreply.github.com>
2018-01-27 12:47:08 -0200

    player andando bonitinho

--- Script/Player.gd
+++ Script/Player.gd
@@ -10,12 +10,14 @@ const GRAVITY = 2000
 const JUMPS_PEED = -1000
 
 var acc = Vector2()
-var vel = Vector2()
+var vel = Vector2(0,0)
 var anim = "parada"
-sprite.play("anim")
+
 
 
 func _ready():
+	get_node("SamplePlayer").play("energyShield", false)
+	#get_node("SamplePlayer").play("creepyMoaning", false)
 	set_fixed_process(true)
 
 func _fixed_process(delta):
@@ -25,7 +27,13 @@ func _fixed_process(delta):
 	acc.x *= ACCEL
 	if acc.x == 0:
 		acc.x = vel.x * FRICTION * delta
+		
+	else:
+		get_node("SamplePlayer").play("concreteStep", false)
+		
 	if Input.is_action_pressed("pula") and ground_ray.is_colliding():
+		get_node("SamplePlayer").play("footStepWoodsBit", false)
+		print("pula")
 		vel.y = JUMPS_PEED
 	vel += acc * delta
 	vel.x = clamp(vel.x, -MAX_SPEED, MAX_SPEED)
@@ -35,14 +43,17 @@ func _fixed_process(delta):
 		motion = n.slide(motion)
 		vel = n.slide(vel)
 		move(motion)
+	if abs(vel.x) < 10:
+		vel.x = 0
 	
 	if vel.x == 0:
 		anim = "parada"
+		print(vel.x)
 	elif vel.x > 0:
 		anim = "correndo"
 		sprite.set_flip_h(false)
 	elif vel.x < 0:
 		anim = "correndo"
 		sprite.set_flip_h(true)
-	sprite.play("anim")
+	sprite.play(anim)
 	
