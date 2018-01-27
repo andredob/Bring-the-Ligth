264216f4f560faa49e70748544eea79a77768297
dequevedo <danielelvisquevedo@gmail.com>
2018-01-27 18:37:50 -0200

    Template de fantasma adicionado

--- Script/Missile.gd
+++ Script/Missile.gd
@@ -1,6 +1,6 @@
 extends Sprite
 
-onready var player = get_parent().get_node("Player")
+var target
 var speed = 200
 
 func _ready():
@@ -8,7 +8,11 @@ func _ready():
 	pass
 
 func _process(delta):
-	var playerPos = player.get_global_pos()
-	set_rot(get_pos().angle_to_point(playerPos))
+	var targetPos = target.get_global_pos()
+	set_rot(get_pos().angle_to_point(targetPos))
 	move_local_y(delta * -speed, false)
+	
+	var distanceFromLeecher = get_global_pos().distance_to(target.get_global_pos());
+	if(distanceFromLeecher <= 2): 
+		queue_free()
 	
