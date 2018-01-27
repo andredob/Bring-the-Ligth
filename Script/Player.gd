076f03033af7988a4beddddcb3e0b2bcf624787f
andredob <32962579+andredob@users.noreply.github.com>
2018-01-27 11:43:35 -0200

    corrigido

--- Script/Player.gd
+++ Script/Player.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 onready var ground_ray = get_node("RayCast2D")
+onready var sprite = get_node("AnimatedSprite")
 
 const ACCEL = 1500
 const MAX_SPEED = 500
@@ -10,7 +11,8 @@ const JUMPS_PEED = -1000
 
 var acc = Vector2()
 var vel = Vector2()
-
+var anim = "parada"
+sprite.play("anim")
 
 
 func _ready():
@@ -19,6 +21,7 @@ func _ready():
 func _fixed_process(delta):
 	acc.y = GRAVITY
 	acc.x = Input.is_action_pressed("direita") - Input.is_action_pressed("esquerda")
+	
 	acc.x *= ACCEL
 	if acc.x == 0:
 		acc.x = vel.x * FRICTION * delta
@@ -32,3 +35,14 @@ func _fixed_process(delta):
 		motion = n.slide(motion)
 		vel = n.slide(vel)
 		move(motion)
+	
+	if vel.x == 0:
+		anim = "parada"
+	elif vel.x > 0:
+		anim = "correndo"
+		sprite.set_flip_h(false)
+	elif vel.x < 0:
+		anim = "correndo"
+		sprite.set_flip_h(true)
+	sprite.play("anim")
+	
