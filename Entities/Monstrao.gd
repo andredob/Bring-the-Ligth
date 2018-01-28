c31e7ae280564f279b0f022d40f05afd9e99e9e5
afeseila <joaobenetasso@hotmail.com>
2018-01-28 12:55:17 -0200

    monstro roxo

--- Entities/Monstrao.gd
+++ Entities/Monstrao.gd
@@ -5,9 +5,11 @@ extends RigidBody2D
 # var b = "textvar"
 var vel =300
 var aux = 1
+onready var init_pos = get_global_pos()
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	get_node("Timer").set_wait_time(5)
 	pass
 	
 func _on_VisibilityNotifier2D_enter_viewport( viewport ):
@@ -20,6 +22,7 @@ func _on_VisibilityNotifier2D_enter_viewport( viewport ):
 func _on_VisibilityNotifier2D_enter_screen():
 	
 	set_fixed_process(true)
+	print("entrou tela monstrao")
 	
 
 
@@ -29,10 +32,22 @@ func _fixed_process(delta):
 	aux += 0.05
 	#get_node("Sprite").set_rot(aux)
 	
+	pass
 
 
 
 
 func _on_VisibilityNotifier2D_exit_screen():
-	queue_free()
-	#print("saiu tela")
+	get_node("Timer").start()
+	set_fixed_process(false)
+	print("saiu tela monstrao")
+
+
+func _on_Timer_enter_tree():
+	pass # replace with function body
+
+
+func _on_Timer_timeout():
+	print ("renasceu")
+	set_global_pos(init_pos)
+	pass # replace with function body
