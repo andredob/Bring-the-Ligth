d8ddab275f02500af5c8a9ef7cf84601246c0ba4
taciobn <taciobarreto@hotmail.com>
2018-01-28 08:49:49 -0200

    final

---
+++ Scripts/Poder.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+var vel = 500
+
+var dir = Vector2(1,0)
+func _ready():
+	set_process(true)
+	pass
+	
+func _process(delta):
+	set_pos(get_pos()+dir*vel*delta)
+	
+	if( get_global_pos().distance_to(get_parent().get_node("player").get_global_pos()) >=750  ):
+		queue_free()
+	
+	pass
+
