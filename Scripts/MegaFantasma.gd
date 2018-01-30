0e106568d318ba838646f7c4931877afe0b55b96
taciobn <taciobarreto@hotmail.com>
2018-01-30 01:12:10 -0200

    senario

--- Scripts/MegaFantasma.gd
+++ Scripts/MegaFantasma.gd
@@ -1,13 +1,16 @@
 extends KinematicBody2D
 const DANO = 50
 var vel = 1000
-
+var time = 0
 func _ready():
 	hide()
 	pass
 	
 func _fixed_process(delta):
 	translate(Vector2(get_scale().x,0)*vel*delta)
+	time +=1
+	if time == 5*60:
+		queue_free()
 	pass
 
 
